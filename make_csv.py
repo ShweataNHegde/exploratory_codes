ba5bdd649ef174f2d1ec30b2df9a4749357dffae
Shweata N Hegde <shweata.hegde@gmail.com>
2021-08-27 16:12:55 +0530

    minor changes

--- make_csv.py
+++ make_csv.py
@@ -1,11 +1,6 @@
 import gzip
 import csv
 import os
-import itertools
-import copy
-import networkx as nx
-import pandas as pd
-import matplotlib.pyplot as plt
 
 def unzip_tsv_gz_and_write_to_csv(tsv_gz_path, csv_path):
     with gzip.open(tsv_gz_path, 'rt') as f:
@@ -23,7 +18,4 @@ csv_path_weights = os.path.join('resources', 'butterfly_weights.csv')
 unzip_tsv_gz_and_write_to_csv('resources\SS-Butterfly_weights.tsv.gz', 
                             csv_path_weights)
 
-edge_df = pd.read_csv(csv_path_weights)
-node_df = pd.read_csv(csv_path_labels)
-g = nx.Graph()
-g.add_node_from(node_df['# Node_ID'])
+
